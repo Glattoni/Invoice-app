16db96cbb9e61ea2e51a478d0de25ed5b2ef2c25
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-01-03 12:06:58 +0200

    #1 Enhancement Add light and dark theme support (#2)
    * enhancement: add light and dark theme support
    
    * feat(CI): run workflow action for develop branch

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  theme: Theme = 'light-theme';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    this.initializeTheme();
+  }
+
+  initializeTheme(): void {
+    this.renderer.addClass(this.document.body, this.theme);
+  }
 }
+
+export type Theme = 'light-theme' | 'dark-theme';
