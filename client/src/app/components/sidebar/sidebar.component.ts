b6256f07a983629dfd1425ac617e5c8184c94b8e
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-01-13 12:36:17 +0200

    enhancement: refactor project structure (#14)
    * feat(stylelint): ignore angular pseudo elements
    
    * chore: change plus icon
    
    * enhance: refactor root structure

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ThemeService } from '@features/theme/theme.service';
+import { ThemeService } from 'src/app/theme/theme.service';
 
 @Component({
   selector: 'app-sidebar',
