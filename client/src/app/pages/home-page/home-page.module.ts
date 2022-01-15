6b5972037859af4bbd9aebbc577c4a9151f153a5
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-01-15 15:39:35 +0200

    enhance: add dropdown filter options list (#16)
    * feat(themes): extend color palette
    
    * chore: add new global styles
    
    * enhance: add dropdown filter options list

--- client/src/app/pages/home-page/home-page.module.ts
+++ client/src/app/pages/home-page/home-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { ButtonsModule } from '@shared/buttons/buttons.module';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { HeaderModule } from '@features/header/header.module';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, ButtonsModule, HomePageRoutingModule],
+  imports: [CommonModule, ButtonsModule, HomePageRoutingModule, HeaderModule],
 })
 export class HomePageModule {}
