770a04b9d6527c92a002b4a70cd05ecc27447247
Glattoni <glattoni8@gmail.com>
2022-01-16 19:12:06 +0200

    enhance: add invoice state component prototype

--- client/src/app/pages/home-page/home-page.module.ts
+++ client/src/app/pages/home-page/home-page.module.ts
@@ -4,9 +4,16 @@ import { HomePageComponent } from './home-page.component';
 import { ButtonsModule } from '@shared/buttons/buttons.module';
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HeaderModule } from '@features/header/header.module';
+import { InvoiceStateModule } from '@shared/invoice-state/invoice-state.module';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, ButtonsModule, HomePageRoutingModule, HeaderModule],
+  imports: [
+    CommonModule,
+    ButtonsModule,
+    HomePageRoutingModule,
+    HeaderModule,
+    InvoiceStateModule,
+  ],
 })
 export class HomePageModule {}
---
+++ client/src/app/shared/invoice-state/invoice-state.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-state',
+  templateUrl: './invoice-state.component.html',
+  styleUrls: ['./invoice-state.component.scss'],
+})
+export class InvoiceStateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
