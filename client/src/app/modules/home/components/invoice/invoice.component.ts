5c71ccdeb2ad426ddcdf417c358cb53990921a0c
Glattoni <glattoni8@gmail.com>
2022-01-19 17:50:16 +0200

    feat(invoice): allow component to accept props

--- client/src/app/modules/home/components/invoice/invoice.component.ts
+++ client/src/app/modules/home/components/invoice/invoice.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.scss']
+  styleUrls: ['./invoice.component.scss'],
 })
 export class InvoiceComponent implements OnInit {
+  @Input() clientName: string = '';
+  @Input() paymentDue: string = '';
+  @Input() total: string = '';
+  @Input() status: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
