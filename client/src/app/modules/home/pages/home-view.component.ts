65dca51fe4a76c9c06f6e87eb93a3fd43ed611f5
Glattoni <glattoni8@gmail.com>
2022-01-19 20:57:09 +0200

    chore: add path to core directory

--- client/src/app/modules/home/pages/home-view.component.ts
+++ client/src/app/modules/home/pages/home-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { InvoiceService } from 'src/app/core/services/invoice.service';
+import { InvoiceService } from '@core/services/invoice.service';
 
 @Component({
   selector: 'app-home-view',
