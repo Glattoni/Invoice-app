b2d93ba58db0605c66eed99d874ae01331a11635
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-01-19 21:00:26 +0200

    enhancement: fetch data from db and display it on the home page (#29)
    * fix: solve CORS error
    
    * enhance: add custom date pipe
    
    * fix(invoice-state): set proper `variant` type
    
    * enhance: add service for fetching data from db
    
    * feat(header): display real number of invoices
    
    * feat(invoice): allow component to accept props
    
    * feat(home page): pass data from db to `app-invoice`
    
    * chore: add path to core directory
    
    * fix: import HttpClientTestingModule for testing

--- client/src/app/modules/home/pages/home-view.component.spec.ts
+++ client/src/app/modules/home/pages/home-view.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeViewComponent } from './home-view.component';
@@ -8,9 +9,9 @@ describe('HomeViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeViewComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeViewComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- client/src/app/modules/home/pages/home-view.component.ts
+++ client/src/app/modules/home/pages/home-view.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from '@core/services/invoice.service';
 
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.component.html',
-  styleUrls: ['./home-view.component.scss']
+  styleUrls: ['./home-view.component.scss'],
 })
 export class HomeViewComponent implements OnInit {
-
-  constructor() { }
+  invoices: any;
+  constructor(private invoiceService: InvoiceService) {}
 
   ngOnInit(): void {
+    this.getInvoices();
   }
 
+  getInvoices(): void {
+    this.invoiceService
+      .getInvoices()
+      .subscribe((value) => (this.invoices = value.invoices));
+  }
 }
