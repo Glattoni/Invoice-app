d90c9e48403d68a6c54395433245d0a6bdcaab84
Glattoni <glattoni8@gmail.com>
2022-01-28 22:14:40 +0200

    fix: all missing testing modules

--- client/src/app/modules/invoice-detail/pages/invoice-detail-page.component.spec.ts
+++ client/src/app/modules/invoice-detail/pages/invoice-detail-page.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { InvoiceDetailPageComponent } from './invoice-detail-page.component';
 
@@ -8,9 +10,9 @@ describe('InvoiceDetailPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InvoiceDetailPageComponent ]
-    })
-    .compileComponents();
+      declarations: [InvoiceDetailPageComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
