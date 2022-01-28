ab24d6885d4bdea4079c8e579199eb46f8ed2dcb
Glattoni <glattoni8@gmail.com>
2022-01-28 12:48:54 +0200

    enhance: display mock invoice detail

--- client/src/app/modules/invoice-detail/invoice-detail.module.ts
+++ client/src/app/modules/invoice-detail/invoice-detail.module.ts
@@ -5,9 +5,18 @@ import { SharedModule } from '@shared/shared.module';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { InvoiceDetailPageComponent } from './pages/invoice-detail-page.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { TableComponent } from './components/table/table.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [InvoiceDetailPageComponent, HeaderComponent],
+  declarations: [
+    InvoiceDetailPageComponent,
+    HeaderComponent,
+    BodyComponent,
+    TableComponent,
+    FooterComponent,
+  ],
   imports: [
     CommonModule,
     InvoiceDetailRoutingModule,
