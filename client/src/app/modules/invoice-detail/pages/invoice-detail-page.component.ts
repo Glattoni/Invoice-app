87ed5753d9902322239a96298a22860e8b139ee0
Glattoni <glattoni8@gmail.com>
2022-02-05 17:51:37 +0200

    feat(invoice-detail): setup invoice deletion

--- client/src/app/modules/invoice-detail/pages/invoice-detail-page.component.ts
+++ client/src/app/modules/invoice-detail/pages/invoice-detail-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Invoice } from '@shared/models/invoice.model';
 import { InvoiceService } from '@shared/services/invoice/invoice.service';
+import { ModalService } from '@shared/services/modal/modal.service';
 
 @Component({
   templateUrl: './invoice-detail-page.component.html',
@@ -12,7 +13,9 @@ export class InvoiceDetailPageComponent implements OnInit {
   @Input() invoice?: Invoice;
   constructor(
     private route: ActivatedRoute,
-    private invoiceService: InvoiceService
+    private router: Router,
+    private invoiceService: InvoiceService,
+    private modalService: ModalService
   ) {}
 
   ngOnInit(): void {
@@ -24,5 +27,22 @@ export class InvoiceDetailPageComponent implements OnInit {
     this.invoiceService
       .getInvoice(id)
       .subscribe((invoice) => (this.invoice = invoice));
+  }
+
+  deleteInvoice(): void {
+    const id = String(this.route.snapshot.paramMap.get('slug'));
+    this.invoiceService.deleteInvoice(id).subscribe();
+    this.router.navigate(['']);
+  }
+
+  //TODO refresh the route if the data does not change after invoice deletion
+  /* reloadCurrentRoute(): void {
+    this.router
+      .navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigate(['']));
+  } */
+
+  closeModal(id: string) {
+    this.modalService.close(id);
   }
 }
