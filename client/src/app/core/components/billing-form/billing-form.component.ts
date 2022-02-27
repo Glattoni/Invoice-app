5eb17c06bb0c856ac3370b0ef929aea9e9043744
Glattoni <glattoni8@gmail.com>
2022-02-27 12:46:56 +0200

    enhance: add form text field

---
+++ client/src/app/core/components/billing-form/billing-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-form',
+  templateUrl: './billing-form.component.html',
+  styleUrls: ['./billing-form.component.scss'],
+})
+export class BillingFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
