16fd536f2dc56711f06fb9eec56b402005639668
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-03-05 18:22:50 +0200

    feat: add custom select (#51)

--- client/src/app/core/components/billing-form/billing-form.component.ts
+++ client/src/app/core/components/billing-form/billing-form.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-billing-form',
   templateUrl: './billing-form.component.html',
   styleUrls: ['./billing-form.component.scss'],
 })
-export class BillingFormComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class BillingFormComponent {
+  readonly options: any = [
+    { id: '1', label: 'Next 1 day' },
+    { id: '2', label: 'Next 7 days' },
+    { id: '3', label: 'Next 14 days' },
+    { id: '4', label: 'Next 30 days' },
+  ];
 }
