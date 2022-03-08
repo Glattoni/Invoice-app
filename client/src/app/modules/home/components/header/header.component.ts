95b254b44df8df997bd9e9bf338a4e483ced16a1
Glattoni <glattoni8@gmail.com>
2022-03-08 16:02:29 +0200

    enhance(header): update description after invoice filtering

--- client/src/app/modules/home/components/header/header.component.ts
+++ client/src/app/modules/home/components/header/header.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
-import { Invoice } from '@shared/models/invoice.model';
+import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from '@shared/services/invoice/invoice.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  invoices$: Observable<Invoice[]>;
+export class HeaderComponent implements OnInit {
+  amount: number = 0;
+  filter: string = '';
 
-  constructor(private invoiceService: InvoiceService) {
-    this.invoices$ = this.invoiceService.invoices$;
+  constructor(private invoiceService: InvoiceService) {}
+
+  ngOnInit(): void {
+    this.invoiceService.filteredInvoices$.subscribe(
+      (value) => (this.amount = value.length)
+    );
+    this.invoiceService.currentFilter$.subscribe(
+      (value) => (this.filter = value)
+    );
+  }
+
+  get description() {
+    let result;
+
+    if (this.amount === 0) {
+      result = 'No invoices';
+    }
+    if (!this.filter) {
+      result = `There are ${this.amount} total invoices`;
+    }
+    if (!this.filter && this.amount === 1) {
+      result = `There is 1 total invoice`;
+    }
+    if (this.filter && this.amount === 1) {
+      result = `There is 1 ${this.filter} invoice`;
+    }
+    if (this.filter && this.amount > 1) {
+      result = `There are ${this.amount} ${this.filter} invoices`;
+    }
+
+    return result;
   }
 }
