c2a4bdedffc325efbab0ee05f5587a8eee11d0a7
Glattoni <glattoni8@gmail.com>
2022-03-19 17:04:47 +0200

    feat(billing-form): move to home module

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,14 +2,11 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { BillingFormComponent } from './components/billing-form/billing-form.component';
-import { TextFieldComponent } from './components/form-elements/text-field/text-field.component';
-import { DropdownComponent } from './components/form-elements/dropdown/dropdown.component';
 
 @NgModule({
-  declarations: [SidebarComponent, BillingFormComponent, TextFieldComponent, DropdownComponent],
+  declarations: [SidebarComponent],
   imports: [CommonModule, AngularSvgIconModule],
-  exports: [SidebarComponent, BillingFormComponent],
+  exports: [SidebarComponent],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
