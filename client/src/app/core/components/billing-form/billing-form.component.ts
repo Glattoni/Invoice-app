0f1e4d5a3aaf91d6650f97c77a2e3ee8fe28a3ea
Glattoni <glattoni8@gmail.com>
2022-04-13 09:35:12 +0300

    feat(billing form): submit form with user data

--- client/src/app/core/components/billing-form/billing-form.component.ts
+++ client/src/app/core/components/billing-form/billing-form.component.ts
@@ -1,19 +1,39 @@
-import { distinctUntilChanged, Subscription } from 'rxjs';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  OnInit,
+  OnDestroy,
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+import {
+  Subject,
+  takeUntil,
+  fromEvent,
+  throttleTime,
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs';
+
+import { addDays, generateSlug } from 'src/utils';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
-import { ScrollService } from '@core/services/scroll/scroll.service';
+import { InvoiceService } from '@core/services/invoice/invoice.service';
 import { SidebarFormService } from '@core/services/sidebar-form/sidebar-form.service';
 
 @Component({
   selector: 'app-billing-form',
   templateUrl: './billing-form.component.html',
   styleUrls: ['./billing-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BillingFormComponent implements OnInit, OnDestroy {
+export class BillingFormComponent implements OnInit, OnDestroy, AfterViewInit {
   billingForm: FormGroup;
   isBottom: boolean = false;
-  scrollSubscription: Subscription;
+  submitted: boolean = false;
+  private destroy$ = new Subject<void>();
 
   readonly options: any = [
     { id: '1', label: 'Next 1 day' },
@@ -31,41 +51,64 @@ export class BillingFormComponent implements OnInit, OnDestroy {
 
   constructor(
     private fb: FormBuilder,
-    private scrollService: ScrollService,
-    private sidebarFormService: SidebarFormService
+    private sidebarFormService: SidebarFormService,
+    private invoiceService: InvoiceService
   ) {
-    this.scrollSubscription = this.scrollService.isBottom$.subscribe();
     this.billingForm = this.fb.group({
+      slug: generateSlug(),
       senderAddress: this.address,
       clientName: [''],
       clientEmail: [''],
+      status: 'pending',
       clientAddress: this.address,
-      invoiceDate: [''],
-      paymentTerms: [''],
+      createdAt: [''],
+      paymentTerms: ['30'],
+      paymentDue: [''],
       projectDescription: [''],
       items: this.fb.array([]),
       total: 0,
     });
   }
 
+  @ViewChild('formContainer') formContainer: ElementRef | undefined;
+
   ngOnInit(): void {
-    this.billingForm.valueChanges.subscribe((value) => {
-      console.log(value);
-    });
-    this.itemForms.valueChanges.subscribe((_) => {
-      this.calculateTotal();
-    });
-    this.scrollSubscription = this.scrollService.isBottom$
-      .pipe(distinctUntilChanged())
-      .subscribe((value) => (this.isBottom = value));
+    this.billingForm.valueChanges
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((value) => {
+        this.getPaymentDueDate();
+      });
+
+    this.items.valueChanges
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((value) => {
+        this.calculateTotal();
+      });
   }
 
   ngOnDestroy(): void {
-    this.scrollSubscription.unsubscribe();
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
-  get itemForms() {
-    return this.billingForm.get('items') as FormArray;
+  handleScroll(e: any) {
+    return (
+      e.target.offsetHeight + e.target.scrollTop >= e.target.scrollHeight - 100
+    );
+  }
+
+  ngAfterViewInit() {
+    fromEvent(this.formContainer?.nativeElement, 'scroll', this.handleScroll)
+      .pipe(throttleTime(25), takeUntil(this.destroy$))
+      .subscribe((value) => (this.isBottom = value));
   }
 
   addItem(): void {
@@ -76,34 +119,81 @@ export class BillingFormComponent implements OnInit, OnDestroy {
       total: [0],
     });
 
-    this.itemForms.push(item);
+    this.items.push(item);
   }
 
   deleteItem(idx: number): void {
-    this.itemForms.removeAt(idx);
+    this.items.removeAt(idx);
   }
 
   calculateItemTotal(idx: number) {
-    const item = this.itemForms.controls[idx];
+    const item = this.items.controls[idx];
     const quantity = parseInt(item.get('quantity')?.value);
     const price = parseInt(item.get('price')?.value);
-    const totalPrice = quantity * price || '';
-    item.get('total')?.setValue(totalPrice);
+    const itemTotal = quantity * price || 0;
+    item.get('total')?.setValue(itemTotal);
 
-    return totalPrice;
+    return itemTotal;
   }
 
   calculateTotal(): void {
-    const grandTotal = this.billingForm.get('total');
-    const amountDue = this.itemForms.controls
+    const grandTotal = this.items.controls
       .map((c) => parseInt(c.get('total')?.value))
       .reduce((a, b) => a + b);
 
-    grandTotal?.setValue(amountDue);
+    this.total?.setValue(grandTotal);
+  }
+
+  getPaymentDueDate(): void {
+    const date = this.createdAt?.value;
+    const amount = this.paymentTerms?.value;
+    const due = addDays(date, amount);
+
+    this.paymentDue?.setValue(due, { onlySelf: true });
   }
 
   discardForm(): void {
     this.billingForm.reset();
     this.sidebarFormService.close();
+  }
+
+  saveAsDraft(): void {
+    this.status?.setValue('draft');
+    this.onSubmit();
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    this.invoiceService.createInvoice(this.formData);
+    this.billingForm.reset();
+    this.sidebarFormService.close();
+  }
+
+  get formData() {
+    return this.billingForm.value;
+  }
+
+  get total() {
+    return this.billingForm.get('total');
+  }
+
+  get status() {
+    return this.billingForm.get('status');
+  }
+
+  get createdAt() {
+    return this.billingForm.get('createdAt');
+  }
+
+  get paymentDue() {
+    return this.billingForm.get('paymentDue');
+  }
+
+  get paymentTerms() {
+    return this.billingForm.get('paymentTerms');
+  }
+
+  get items() {
+    return this.billingForm.get('items') as FormArray;
   }
 }
