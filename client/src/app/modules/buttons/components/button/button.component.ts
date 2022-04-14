2f90ec9c18f028aaf290c2f9149727022d6826cd
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-04-14 08:18:48 +0300

    feature: add billing form submittion (#78)
    * feat(stylelint): allow vendor prefixes
    
    * chore: remove unnecessary service
    
    * feat(invoice service): add new method & make code more declarative
    
    * feat(app button): add new props
    
    * feat(billing form): submit form with user data
    
    * chore: updated dependencies
    
    * chore: minor improvements
    
    * feat(server): add new field to mongoose schema
    
    * chore: add missing test import

--- client/src/app/modules/buttons/components/button/button.component.ts
+++ client/src/app/modules/buttons/components/button/button.component.ts
@@ -7,7 +7,9 @@ import { Button } from '../../types/button';
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
-  @Input() hasIcon: boolean = false;
   @Input() variant: Button = 'primary';
+  @Input() disabled: boolean = false;
+  @Input() hasIcon: boolean = false;
   @Input() small: boolean = false;
+  @Input() type: string = 'button';
 }
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,24 +1,31 @@
-const getRandomString = (length: number) => {
-  let result = '';
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  for (let i = 0; i < length; i++) {
-    result += alphabet[Math.floor(Math.random() * alphabet.length)];
-  }
-  return result;
-};
+enum Time {
+  Hours = 24,
+  Minutes = 60,
+  Seconds = 60,
+  Milliseconds = 1000,
+}
 
-const getRandomNumber = (length: number) => {
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += Math.floor(Math.random() * 10);
-  }
-  return result;
-};
+const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-const generateSlug = () => {
-  const characters = getRandomString(2);
-  const number = getRandomNumber(4);
-  return `${characters}${number}`;
-};
+const numToArr = (n: number) => Array.from(Array(n));
 
-export { generateSlug };
+const msInDays = (amount: number) =>
+  amount * Time.Hours * Time.Minutes * Time.Seconds * Time.Milliseconds;
+
+const getInteger = (length: number) => Math.floor(Math.random() * length);
+
+const getRandomNumber = (digits: number) =>
+  numToArr(digits).reduce((a) => a + getInteger(10), '');
+
+const getRandomString = (length: number) =>
+  numToArr(length).reduce((a) => a + ALPHABET[getInteger(ALPHABET.length)], '');
+
+export const generateSlug = () => `${getRandomString(2)}${getRandomNumber(4)}`;
+
+export const addDays = (date: string, amount: string) => {
+  if (!date) return false;
+  const time = new Date(date).getTime();
+  const milliseconds = time + msInDays(+amount);
+
+  return new Date(milliseconds).toISOString().slice(0, 10);
+};
