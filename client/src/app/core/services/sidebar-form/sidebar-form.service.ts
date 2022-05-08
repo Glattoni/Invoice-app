0040d3fdfdeba3e37cbdaedc2c3cdb961e87b466
Glattoni <glattoni8@gmail.com>
2022-05-08 17:08:33 +0300

    feat(form service): allow passing editable invoice to billing form

--- client/src/app/core/services/sidebar-form/sidebar-form.service.ts
+++ client/src/app/core/services/sidebar-form/sidebar-form.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { Invoice } from '@shared/models/invoice.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SidebarFormService {
   private visible = new BehaviorSubject<boolean>(false);
-  readonly visible$ = this.visible.asObservable();
+  private payload = new ReplaySubject<Invoice>();
 
-  open(): void {
+  readonly visible$ = this.visible.asObservable();
+  readonly payload$ = this.payload.asObservable();
+
+  open(clean?: boolean): void {
+    clean && this.payload.next(null as any);
     this.visible.next(true);
   }
 
   close(): void {
     this.visible.next(false);
+  }
+
+  openForEditing(invoice: Invoice): void {
+    this.payload.next(invoice);
+    this.open();
+  }
+
+  finishEditing(): void {
+    this.payload.next(null as any);
+    this.close();
   }
 }
