e104117560d74c47d5c1e0f58e9961fd778238a2
Glattoni <glattoni8@gmail.com>
2022-05-08 11:44:31 +0300

    feat(invoice detail): prefer default "date" pipe over custom one

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,24 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './components/modal/modal.component';
 import { InvoiceStateComponent } from './components/invoice-state/invoice-state.component';
 
-import { DatePipe } from './pipes/date/date.pipe';
-
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ClickedOutsideDirective } from './directives/clicked-outside.directive';
 
 @NgModule({
   declarations: [
-    DatePipe,
     ModalComponent,
     InvoiceStateComponent,
     ClickedOutsideDirective,
   ],
   imports: [CommonModule, AngularSvgIconModule],
-  exports: [
-    DatePipe,
-    ModalComponent,
-    InvoiceStateComponent,
-    ClickedOutsideDirective,
-  ],
+  exports: [ModalComponent, InvoiceStateComponent, ClickedOutsideDirective],
 })
 export class SharedModule {}
