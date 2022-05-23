0d8d7713a0808cea3ac492267d6e061e20034c0d
Glattoni <glattoni8@gmail.com>
2022-05-23 09:46:49 +0300

    chore: remove unused old code

--- client/src/app/modules/home/home.module.ts
+++ client/src/app/modules/home/home.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { AngularSvgIconModule } from 'angular-svg-icon';
 import { SharedModule } from '@shared/shared.module';
-import { InvoiceComponent } from './components/invoice/invoice.component';
-import { PlaceholderComponent } from './components/placeholder/placeholder.component';
-import { HomePageComponent } from './pages/home/home-page.component';
-import { SummaryPipe } from './pipes/summary/summary.pipe';
-import { PortalModule } from '@angular/cdk/portal';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ButtonsModule } from '../buttons/buttons.module';
+import { HomeRoutingModule } from './home-routing.module';
 import { BillingFormModule } from '@modules/billing-form/billing-form.module';
+
+import { SummaryPipe } from './pipes/summary/summary.pipe';
+import { HomePageComponent } from './pages/home/home-page.component';
+
+import { HeaderComponent } from './components/header/header.component';
+import { InvoiceComponent } from './components/invoice/invoice.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,6 @@ import { BillingFormModule } from '@modules/billing-form/billing-form.module';
   imports: [
     CommonModule,
     SharedModule,
-    PortalModule,
     ButtonsModule,
     HomeRoutingModule,
     BillingFormModule,
