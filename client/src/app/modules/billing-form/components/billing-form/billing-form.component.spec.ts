b712af8c3179f79c8287e030dc46f30cd1dd7886
Glattoni <glattoni8@gmail.com>
2022-06-09 09:07:58 +0300

    chore: fix imports

--- client/src/app/modules/billing-form/components/billing-form/billing-form.component.spec.ts
+++ client/src/app/modules/billing-form/components/billing-form/billing-form.component.spec.ts
@@ -4,10 +4,10 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormGroupDirective, NonNullableFormBuilder } from '@angular/forms';
 
 import { BillingFormComponent } from './billing-form.component';
-import { ItemsListComponent } from './components/items-list/items-list.component';
-import { ClientInfoComponent } from './components/client-info/client-info.component';
-import { InvoiceTermsComponent } from './components/invoice-terms/invoice-terms.component';
-import { SenderAddressComponent } from './components/sender-address/sender-address.component';
+import { ItemsListComponent } from '../items-list/items-list.component';
+import { ClientInfoComponent } from '../client-info/client-info.component';
+import { InvoiceTermsComponent } from '../invoice-terms/invoice-terms.component';
+import { SenderAddressComponent } from '../sender-address/sender-address.component';
 
 import { ButtonComponent } from '@modules/buttons/components/button/button.component';
 import { GoBackButtonComponent } from '@modules/buttons/components/go-back-button/go-back-button.component';
