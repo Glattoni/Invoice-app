5d66d93682064ec98e2e46c24a29949eaa14ab0d
Glattoni <glattoni8@gmail.com>
2022-06-11 13:12:06 +0300

    feat(app component): remove form logic

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,8 @@
-import { Observable } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
-import { SidebarFormService } from '@core/services/sidebar-form/sidebar-form.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  visible$?: Observable<boolean>;
-
-  constructor(private sidebarFormService: SidebarFormService) {}
-
-  ngOnInit(): void {
-    this.visible$ = this.sidebarFormService.visible$;
-  }
-
-  closeSidebar(): void {
-    this.sidebarFormService.close();
-  }
-}
+export class AppComponent {}
