5ff03e978bf727c5fedd60ed96fc6390d2c828bb
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-06-22 09:35:08 +0300

    refactor(theme service): prefer data attributes over old implementation (#145)
    * refactor(theme service): prefer data attributes over old implementation
    
    * chore: remove unused css

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from '@core/services/theme/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  theme$?: Observable<'light' | 'dark'>;
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.theme$ = this.themeService.theme$;
+  }
+}
