f78d382f2f6a5b16f46cc5ab83dacdf099390bca
Glattoni <glattoni8@gmail.com>
2022-06-24 12:49:49 +0300

    enhance: add new dialog component

---
+++ client/src/app/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,43 @@
+import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { ModalService } from '@core/services/modal/modal.service';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss'],
+})
+export class DialogComponent implements OnInit, OnDestroy {
+  @Input() id: string = 'dialog';
+
+  constructor(
+    private modalService: ModalService,
+    private element: ElementRef<HTMLDialogElement>
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.id) {
+      throw new Error('Modal must have an id');
+    }
+
+    this.modalService.add(this);
+  }
+
+  ngOnDestroy(): void {
+    this.modalService.remove(this.id);
+  }
+
+  onClick(event: MouseEvent): void {
+    const rect = this.dialog.getBoundingClientRect();
+    const insideDialog =
+      rect.top <= event.clientY &&
+      event.clientY <= rect.top + rect.height &&
+      rect.left <= event.clientX &&
+      event.clientX <= rect.left + rect.width;
+
+    !insideDialog && this.dialog.close();
+  }
+
+  get dialog() {
+    return this.element.nativeElement.children[0] as HTMLDialogElement;
+  }
+}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ModalComponent } from './components/modal/modal.component';
 import { InvoiceStateComponent } from './components/invoice-state/invoice-state.component';
 
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ClickedOutsideDirective } from './directives/clicked-outside.directive';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { ButtonsModule } from '@modules/buttons/buttons.module';
 
 @NgModule({
   declarations: [
-    ModalComponent,
+    DialogComponent,
     InvoiceStateComponent,
     ClickedOutsideDirective,
   ],
-  imports: [CommonModule, AngularSvgIconModule],
-  exports: [ModalComponent, InvoiceStateComponent, ClickedOutsideDirective],
+  imports: [CommonModule, AngularSvgIconModule, ButtonsModule],
+  exports: [DialogComponent, InvoiceStateComponent, ClickedOutsideDirective],
 })
 export class SharedModule {}
