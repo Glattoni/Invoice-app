85f9aa1bab57fe4cd4d36a768abe162356fe8c21
Glattoni <glattoni8@gmail.com>
2022-06-28 11:54:51 +0300

    refactor(custom select): change selected value implementation

--- client/src/app/modules/form-controls/form-controls.module.ts
+++ client/src/app/modules/form-controls/form-controls.module.ts
@@ -5,11 +5,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '@shared/shared.module';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
+import { PaymentTermsPipe } from './pipes/payment-terms.pipe';
 import { CustomInputComponent } from './components/custom-input/custom-input.component';
 import { CustomSelectComponent } from './components/custom-select/custom-select.component';
 
 @NgModule({
-  declarations: [CustomInputComponent, CustomSelectComponent],
+  declarations: [CustomInputComponent, CustomSelectComponent, PaymentTermsPipe],
   imports: [
     FormsModule,
     CommonModule,
