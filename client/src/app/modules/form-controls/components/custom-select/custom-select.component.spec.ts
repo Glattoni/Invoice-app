f3cfde84b68cb3662fae10e5bd38c95637235c52
Glattoni <glattoni8@gmail.com>
2022-07-01 10:12:29 +0300

    chore(custom select): add missing testing import

--- client/src/app/modules/form-controls/components/custom-select/custom-select.component.spec.ts
+++ client/src/app/modules/form-controls/components/custom-select/custom-select.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PaymentTermsPipe } from '@modules/form-controls/pipes/payment-terms.pipe';
 
 import { CustomSelectComponent } from './custom-select.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CustomSelectComponent', () => {
   let component: CustomSelectComponent;
@@ -10,6 +11,7 @@ describe('CustomSelectComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CustomSelectComponent, PaymentTermsPipe],
+      imports: [BrowserAnimationsModule],
     }).compileComponents();
   });
 
