fe3f8c22c765fd817b97dbda4bbe9951cc6186dd
Glattoni <glattoni8@gmail.com>
2022-08-08 09:45:53 +0300

    feat(custom input): minor improvements

--- client/src/app/modules/form-controls/components/custom-input/custom-input.component.ts
+++ client/src/app/modules/form-controls/components/custom-input/custom-input.component.ts
@@ -1,27 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const INPUT_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => CustomInputComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'app-custom-input',
   templateUrl: './custom-input.component.html',
   styleUrls: ['./custom-input.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: CustomInputComponent,
-      multi: true,
-    },
-  ],
+  providers: [INPUT_VALUE_ACCESSOR],
 })
 export class CustomInputComponent implements ControlValueAccessor {
-  @Input() label: string = '';
-  @Input() type: string = 'text';
-  @Input() invalid?: boolean = false;
-  @Input() placeholder: string = '';
+  @Input() label = '';
+  @Input() type = 'text';
+  @Input() invalid? = false;
+  @Input() placeholder = '';
 
-  value: string = '';
-  onTouched?: () => void;
-  onChange?: (value: string) => void;
+  value = '';
+  onTouched!: () => void;
+  onChange!: (value: string) => void;
 
   writeValue(value: string): void {
     this.value = value;
