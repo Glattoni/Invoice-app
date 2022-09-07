3b70873560398ba2e9fc988da228880d95bf92c5
Glattoni <glattoni8@gmail.com>
2022-09-07 08:58:47 +0300

    chore: delete deprecated code

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,21 +1,6 @@
-enum TimeUnits {
-  Hours = 24,
-  Minutes = 60,
-  Seconds = 60,
-  Milliseconds = 1000,
-}
-
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-export const numberToArray = (number: number) =>
-  Array.from(Array(number).keys());
-
-const msInDays = (amount: number) =>
-  amount *
-  TimeUnits.Hours *
-  TimeUnits.Minutes *
-  TimeUnits.Seconds *
-  TimeUnits.Milliseconds;
+const numberToArray = (number: number) => Array.from(Array(number).keys());
 
 const getInteger = (length: number) => Math.floor(Math.random() * length);
 
@@ -29,20 +14,3 @@ const getRandomString = (length: number) =>
   );
 
 export const generateSlug = () => `${getRandomString(2)}${getRandomNumber(4)}`;
-
-export const addDays = (date: string, amount: number) => {
-  const time = new Date(date).getTime();
-  const milliseconds = time + msInDays(amount);
-
-  return new Date(milliseconds).toISOString().slice(0, 10);
-};
-
-export const range = (start: number, end: number) => {
-  const output: number[] = [];
-
-  for (let index = start; index < end; index++) {
-    output.push(index);
-  }
-
-  return output;
-};
