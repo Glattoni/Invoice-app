10fa235fbdf5e3db8f3600165f029e38739c62fa
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-09-16 09:56:42 +0300

    build: update all packages to latest versions & add tests to invoice summary service (#171)
    * feat(invoice summary service): add tests
    
    * feat(build): update all deps to latest versions
    
    * fix(tsconfig): remove trailing coma
    
    * fix(sonar): remove code smells

--- src/app/modules/home/services/invoice-summary/invoice-summary.service.spec.ts
+++ src/app/modules/home/services/invoice-summary/invoice-summary.service.spec.ts
@@ -15,5 +15,29 @@ describe('InvoiceSummaryService', () => {
     service = TestBed.inject(InvoiceSummaryService);
   });
 
-  it('should...', () => {});
+  it('should transform when amount is 0 and filter is undefined', () => {
+    expect(service['transform'](0)).toBe('No invoices');
+  });
+
+  it('should transform when amount is 0 and filter is set to draft', () => {
+    expect(service['transform'](0, 'draft')).toBe('No invoices');
+  });
+
+  it('should transform when amount is 1 and filter is undefined', () => {
+    expect(service['transform'](1)).toBe('There is 1 total invoice');
+  });
+
+  it('should transform when amount is 1 and filter is present', () => {
+    expect(service['transform'](1, 'paid')).toBe('There is 1 paid invoice');
+  });
+
+  it('should transform when filter is undefined', () => {
+    expect(service['transform'](10)).toBe('There are 10 total invoices');
+  });
+
+  it('should transform when filter is present', () => {
+    expect(service['transform'](10, 'pending')).toBe(
+      'There are 10 pending invoices'
+    );
+  });
 });
