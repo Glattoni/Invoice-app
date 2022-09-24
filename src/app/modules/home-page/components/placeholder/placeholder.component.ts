7d8b90a50967a26498951aa5544746fa390a7710
Glattoni <glattoni8@gmail.com>
2022-09-24 11:59:58 +0300

    feat(placeholder): update styles & minor improvements

--- src/app/modules/home-page/components/placeholder/placeholder.component.ts
+++ src/app/modules/home-page/components/placeholder/placeholder.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-placeholder',
   templateUrl: './placeholder.component.html',
   styleUrls: ['./placeholder.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlaceholderComponent {}
