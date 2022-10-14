517905c92faa74f6b1f7600d77e3ca842450f3fb
Glattoni <glattoni8@gmail.com>
2022-10-14 12:30:12 +0300

    refactor(invoice-detail-page-header): move logic out of component & update styles

--- src/app/modules/invoice-detail-page/components/header/header.component.ts
+++ src/app/modules/invoice-detail-page/components/header/header.component.ts
@@ -1,40 +1,27 @@
-import { Component, Input } from '@angular/core';
+import {
+  Input,
+  Output,
+  Component,
+  EventEmitter,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
-import { Invoice } from '@shared/models/invoice.model';
-
-import { DialogService } from '@core/services/dialog/dialog.service';
-import { InvoiceService } from '@core/services/invoice/invoice.service';
-import { BillingFormService } from '@core/services/billing-form/billing-form.service';
 import { InvoiceStatus } from '@shared/constants/invoice.constants';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  @Input() invoice?: Invoice;
   @Input() public status: InvoiceStatus = 'pending';
 
-  constructor(
-    private dialogService: DialogService,
-    private invoiceService: InvoiceService,
-    private formService: BillingFormService
-  ) {}
+  @Output() public edit = new EventEmitter<void>();
+  @Output() public delete = new EventEmitter<void>();
+  @Output() public markAsPaid = new EventEmitter<void>();
 
-  openDialog(id: string): void {
-    this.dialogService.openDialog(id);
-  }
-
-  openEditingForm(invoice: Invoice): void {
-    this.formService.startEditing(invoice);
-  }
-
-  markAsPaid(id: string): void {
-    this.invoiceService.markAsPaidInvoice(id);
-  }
-
-  get isPaidInvoice() {
-    return this.invoice?.status === 'paid';
+  public get isPaid(): boolean {
+    return this.status === 'paid';
   }
 }
--- src/app/modules/invoice-detail-page/invoice-detail-page.component.ts
+++ src/app/modules/invoice-detail-page/invoice-detail-page.component.ts
@@ -1,31 +1,50 @@
 import { Observable } from 'rxjs';
+
+import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { Invoice } from '@shared/models/invoice.model';
+import { DialogService } from '@core/services/dialog/dialog.service';
 import { InvoiceService } from '@core/services/invoice/invoice.service';
-import { GoBack } from '@modules/buttons/components/go-back-button/go-back-button.component';
+import { BillingFormService } from '@core/services/billing-form/billing-form.service';
 
 @Component({
   templateUrl: './invoice-detail-page.component.html',
   styleUrls: ['./invoice-detail-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InvoiceDetailPageComponent implements OnInit {
-  invoice$?: Observable<Invoice>;
-  link = GoBack.Link;
+  public invoice$: Observable<Invoice>;
 
   constructor(
-    private route: ActivatedRoute,
-    private invoiceService: InvoiceService
-  ) {}
-
-  ngOnInit(): void {
-    this.invoice$ = this.invoiceService.invoice$;
-    this.getInvoice();
+    private readonly route: ActivatedRoute,
+    private readonly location: Location,
+    private readonly dialogService: DialogService,
+    private readonly invoiceService: InvoiceService,
+    private readonly billingFormService: BillingFormService
+  ) {
+    this.invoice$ = invoiceService.invoice$;
   }
 
-  getInvoice(): void {
-    const invoiceId = String(this.route.snapshot.paramMap.get('id'));
-    this.invoiceService.getInvoice(invoiceId);
+  public ngOnInit(): void {
+    const id = String(this.route.snapshot.paramMap.get('id'));
+    this.invoiceService.getInvoice(id);
+  }
+
+  public toPreviousPage(): void {
+    this.location.back();
+  }
+
+  public onEdit(invoice: Invoice): void {
+    this.billingFormService.startEditing(invoice);
+  }
+
+  public onDelete(): void {
+    this.dialogService.openDialog('delete-dialog');
+  }
+
+  public onMarkAsPaid(): void {
+    this.dialogService.openDialog('mark-as-paid-dialog');
   }
 }
