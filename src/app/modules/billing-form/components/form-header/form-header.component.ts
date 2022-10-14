8e133fc8dbb3bae926ed12363cf6cc450e99ddd0
Glattoni <glattoni8@gmail.com>
2022-10-14 11:06:05 +0300

    chore(form-header): minor updates

--- src/app/modules/billing-form/components/form-header/form-header.component.ts
+++ src/app/modules/billing-form/components/form-header/form-header.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { Invoice } from '@shared/models/invoice.model';
 import { Observable } from 'rxjs';
+import {
+  Input,
+  Output,
+  Component,
+  EventEmitter,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { Invoice } from '@shared/models/invoice.model';
 
 @Component({
   selector: 'app-form-header',
   templateUrl: './form-header.component.html',
   styleUrls: ['./form-header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormHeaderComponent {
-  @Input() payload$?: Observable<Invoice>;
+  @Input() public payload$?: Observable<Invoice>;
+  @Output() public goBack = new EventEmitter<void>();
 }
