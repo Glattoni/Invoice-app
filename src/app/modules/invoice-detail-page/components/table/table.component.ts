5744792ee11fb609b3c68a1343ea214445345628
Glattoni <glattoni8@gmail.com>
2022-10-15 11:46:42 +0300

    feat(app-table): update markup & styles

--- src/app/modules/invoice-detail-page/components/table/table.component.ts
+++ src/app/modules/invoice-detail-page/components/table/table.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Item } from '@shared/models/invoice.model';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableComponent {
-  @Input() items?: Item[];
-  @Input() total?: number;
+  @Input() items: Item[] = [];
+  @Input() total: number = 0;
 }
