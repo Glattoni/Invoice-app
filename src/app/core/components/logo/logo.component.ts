67fbdd68874aa379238c648331007760faaa5e83
Glattoni <glattoni8@gmail.com>
2022-10-15 11:45:50 +0300

    feat(app-logo): minor updates

--- src/app/core/components/logo/logo.component.ts
+++ src/app/core/components/logo/logo.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
-  templateUrl: './logo.component.html',
+  template: `
+    <div class="logo">
+      <svg-icon class="logo__icon" src="assets/icons/logo.svg"></svg-icon>
+    </div>
+  `,
   styleUrls: ['./logo.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LogoComponent {}
