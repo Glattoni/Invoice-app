451e23b058f2afbd9a21658935265dba9b2a3fb9
Alexey Parastyuk <70797850+Glattoni@users.noreply.github.com>
2022-10-22 14:28:15 +0300

    chore(jasmine): add schemas & minor changes (#196)

--- src/app/modules/invoice-detail-page/components/delete-dialog/delete-dialog.component.spec.ts
+++ src/app/modules/invoice-detail-page/components/delete-dialog/delete-dialog.component.spec.ts
@@ -1,5 +1,6 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { DeleteDialogComponent } from './delete-dialog.component';
@@ -12,6 +13,7 @@ describe('DeleteDialogComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [DeleteDialogComponent],
       imports: [RouterTestingModule, HttpClientTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
--- src/app/modules/invoice-detail-page/components/mark-as-paid-dialog/mark-as-paid-dialog.component.spec.ts
+++ src/app/modules/invoice-detail-page/components/mark-as-paid-dialog/mark-as-paid-dialog.component.spec.ts
@@ -1,4 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MarkAsPaidDialogComponent } from './mark-as-paid-dialog.component';
@@ -11,6 +12,7 @@ describe('MarkAsPaidDialogComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [MarkAsPaidDialogComponent],
       imports: [HttpClientModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
