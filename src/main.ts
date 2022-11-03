19b71700025797b64d1c308c49c736ad78263e82
Glattoni <glattoni8@gmail.com>
2022-11-03 20:39:21 +0200

    chore(routing): move app routes to a separate file

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppComponent } from 'app/app.component';
-import { environment } from './environments/environment';
-
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./app/home-page/home-page.component').then(
-        (module) => module.HomePageComponent
-      ),
-  },
-  {
-    path: 'invoices/:id',
-    loadComponent: () =>
-      import('./app/invoice-detail-page/invoice-detail-page.component').then(
-        (module) => module.InvoiceDetailPageComponent
-      ),
-  },
-];
+import { ROUTES } from 'app/app.routes';
+import { AppComponent } from 'app/app.component';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
@@ -34,7 +18,7 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
-      RouterModule.forRoot(routes),
+      RouterModule.forRoot(ROUTES),
       AngularSvgIconModule.forRoot(),
       HttpClientModule,
       ReactiveFormsModule,
